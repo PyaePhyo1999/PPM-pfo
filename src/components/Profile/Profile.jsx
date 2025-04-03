60e15ce2c2802f30058e235b445c63885a2082bc
Pyae Phyo Maung <ppm421999@gmail.com>
2025-04-03 22:24:12 +0700

    resume

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,10 +3,11 @@ import './Profile.css'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Profile = () => {
+  const resume = 'https://github.com/PyaePhyo1999/PPM-pfo/blob/main/resume.pdf'
   const profile = new URL('../../assets/pfile8.jpg', import.meta.url).href;
     const viewResume = () => {
       // Directly open the PDF in a new tab
-        window.open('../resume.pdf', '_blank');
+        window.open(resume, '_blank');
     };
     
   return (
